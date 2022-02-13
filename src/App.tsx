6396aaf80a1ae9a1862936394bd503221720a6b0
geminos-panjan <98257842+geminos-panjan@users.noreply.github.com>
2022-02-13 23:06:54 +0900

    メイン画面を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+import { Description } from "./components/description/Description";
+import { SearchWordInputText } from "./components/searchword/SearchWordInputText";
+import { WordListElement } from "./components/wordlist/WordListElement";
+import "./App.css";
+
 function App() {
+  const [searchWord, setSearchWord] = useState('');
   return (
     <div className="App">
+      <div className="header">
+        <h1 className="caption">Regex Dict</h1>
+      </div>
+      <div className="contents">
+        <Description/>
+        <SearchWordInputText value={searchWord} onChange={(e) => { setSearchWord(e.target.value) }} />
+        <WordListElement searchWord={searchWord}/>
+      </div>
+      <div className="footer">&copy; 2022 geminos-panjan</div>
     </div>
   );
 }
