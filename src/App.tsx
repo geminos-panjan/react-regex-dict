99df9c0833c191b18608a7dc3a5f456143794f18
geminos-panjan <98257842+geminos-panjan@users.noreply.github.com>
2022-02-14 17:24:38 +0900

    Githubリンクを追加

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,12 @@ function App() {
         <SearchWordInputText value={searchWord} onChange={(e) => { setSearchWord(e.target.value) }} />
         <WordListElement searchWord={searchWord}/>
       </div>
-      <div className="footer">&copy; 2022 geminos-panjan</div>
+      <div className="footer">&copy;&nbsp;2022&nbsp;
+        <a
+          href="https://github.com/geminos-panjan"
+          target="_blank" rel="noopener noreferrer"
+        >geminos-panjan</a>
+      </div>
     </div>
   );
 }
